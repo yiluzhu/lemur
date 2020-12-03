7999399c3ffb7d0d6cbad4b657754bc074dc1645
Jihad Asad <jihad.asad@toptal.com>
2020-12-04 00:56:59 +0200

    LMR-7: Make generated reports exportable CSV.

--- lemur/static/app/angular/reports/expiring/view/view.js
+++ lemur/static/app/angular/reports/expiring/view/view.js
@@ -85,6 +85,7 @@ angular.module('lemur')
              CertificateApi.getList(url)
                .then(function (data) {
                  params.total(data.total);
+                 $scope.data = data;
                  $defer.resolve(data);
                });
            }
@@ -97,4 +98,15 @@ angular.module('lemur')
        $scope.showFilters = !$scope.showFilters;
      };
      $scope.filterData(30);
+     $scope.getData = function (){
+       return $scope.data.map(entry => {
+         const csvData = {};
+         $scope.filters
+           .filter(filter=>filter.show)
+           .forEach((item) => {
+             csvData[item.field] = entry[item.field];
+           });
+         return csvData;
+       });
+     };
    });
